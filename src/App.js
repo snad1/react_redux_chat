cf82534b6a7ce3ec7a9128b8ed74dcb6cacf1ee5
Solomon Darku <darkusolomon1@gmail.com>
2022-09-15 08:59:28 +0000

    setup minor ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container,Row,Col,Form,Button} from "react-bootstrap";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [message, setMessage] = useState('')
+  const [username, setUsername] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return <Container>
+    <Row className="justify-content-md-center mx-5">
+
+      <Col md="auto">{message}</Col>
+
+    </Row>
+    <Row className="justify-content-md-center mx-5">
+
+      <Col md="9">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3">
+            <Form.Control type="text" placeholder="Start typing" defaultValue={message}
+                          onChange={(e)=> setMessage(e.target.value)} />
+          </Form.Group>
+        </Form>
+      </Col>
+      <Col md="auto">
+        <Button>Send</Button>
+      </Col>
+
+    </Row>
+  </Container>
 }
 
 export default App;
